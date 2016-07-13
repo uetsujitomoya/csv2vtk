a75142b5ebcf4e5c419f0dc09abd07b2a862b22d
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2016-07-13 13:24:47 +0900

    できた

--- kinect3js/csv2vtk.js
+++ kinect3js/csv2vtk.js
@@ -9,7 +9,7 @@ h=0;
 
 var NoseTip=17;//5はあごっぽい？？ 21が正解っぽい?478
 var M=36;
-let N=52;
+let N=48;
 var face = [];
 
 data.forEach(() => {
@@ -20,9 +20,10 @@ data.forEach(() => {
     face[h][i]=[];
     for(j=0;j<3;j++){
       if(j==2){
-        face[h][i][j]=Math.ceil(1000*data[h][i*5+j+13]-1000*data[h][NoseTip*5+2+13]);
+        face[h][i][j]=//Math.ceil(
+          1000*data[h][i*5+j+13]-1000*data[h][NoseTip*5+2+13];
       }else{
-        face[h][i][j]=Math.ceil(1000*data[h][i*5+j+13]);
+        face[h][i][j]=1000*data[h][i*5+j+13];
       }
     }
   }
@@ -33,7 +34,7 @@ let number=0;
 
 face.forEach(()=>{
 
-  let filename ="ff";
+  let filename ="fh";
   let file_name=filename+number+".vtk";
 
   var content="# vtk DataFile Version 2.0\n";
@@ -49,11 +50,11 @@ face.forEach(()=>{
   content +="POLYGONS "+N+" "+N*4+"\n";
   content +="3 0 1 34\n";
 
-  content +="3 1 2 34\n3 2 3 9\n3 2 3 21\n3 2 9 24\n3 4 6 9\n3 4 6 21\n3 4 9 12\n3 4 12 25\n3 4 21 25\n3 5 7 12\n3 5 7 25\n3 5 12 25\n3 6 9 30\n3 6 21 30\n3 7 11 12\n3 7 11 13\n3 7 13 16\n3 7 16 26\n3 7 24 25\n3 7 24 27\n3 7 26 27\n3 8 14 16\n3 8 16 26\n3 8 26 28\n3 9 12 18\n3 9 17 18\n3 9 17 34\n3 10 14 16\n3 10 15 16\n3 10 15 19\n3 10 18 19\n3 11 12 13\n3 12 13 18\n3 13 15 16\n3 13 18 32\n3 18 19 32\n3 20 21 25\n3 20 21 29\n3 20 23 33\n3 20 25 31\n3 20 31 33\n3 22 23 26\n3 22 23 33\n3 22 26 27\n3 23 26 28\n3 24 25 27\n3 25 27 31\n";
+  content +="3 1 2 34\n3 2 3 9\n3 2 3 21\n3 2 9 34\n3 4 6 9\n3 4 6 21\n3 4 9 12\n3 4 12 25\n3 4 21 25\n3 5 7 12\n3 5 7 25\n3 5 12 25\n3 6 9 30\n3 6 21 30\n3 7 11 12\n3 7 11 13\n3 7 13 16\n3 7 16 26\n3 7 24 25\n3 7 24 27\n3 7 26 27\n3 8 14 16\n3 8 16 26\n3 8 26 28\n3 9 12 18\n3 9 17 18\n3 9 17 34\n3 10 14 16\n3 10 15 16\n3 10 15 19\n3 10 18 19\n3 11 12 13\n3 12 13 18\n3 13 15 16\n3 13 18 32\n3 18 19 32\n3 20 21 25\n3 20 21 29\n3 20 23 33\n3 20 25 31\n3 20 31 33\n3 22 23 26\n3 22 23 33\n3 22 26 27\n3 23 26 28\n3 24 25 27\n3 25 27 31\n";
   //とWriteメソッドを呼び出します。非常に簡単です。開いたファイルは必ず閉じます：
-  content+="CELL_DATA "+N+"\nSCALARS cell-scalar float 3\nLOOKUP_TABLE default\n";
+  content+="\nCELL_DATA "+N+"\nSCALARS cell-scalar float 3\nLOOKUP_TABLE default";
   for(i=0;i<N;i++){
-    content+="0.0 1.0 0.3\n";
+    content+="\n0.0 1.0 0.3";
   }
 
 
